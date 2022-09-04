52a73d91b517594053629c772dd083b005189f3c
Rodolfo Nemes <tecnico.rodolfoo@gmail.com>
2022-09-03 23:20:15 -0300

    Switching player each turn

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -16,17 +16,18 @@ public class King extends ChessPiece {
 	public String toString() {
 		return "K";
 	}
-	
+
 	private boolean canMove(Position position) {
-		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
 	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
-Position p = new Position(0, 0);
-		
+
+		Position p = new Position(0, 0);
+
 		// above
 		p.setValues(position.getRow() - 1, position.getColumn());
 		if (getBoard().positionExists(p) && canMove(p)) {
@@ -68,7 +69,7 @@ Position p = new Position(0, 0);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
+
 		// se
 		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
